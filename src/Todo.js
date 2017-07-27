c460045049faf02f8c38840f75656ef9cfe1427a
juergen <shop@whitearrow.de>
2017-07-27 13:05:59 +0200

    geht!

--- src/Todo.js
+++ src/Todo.js
@@ -1,47 +1,45 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, TouchableHighlight, ListView} from 'react-native';
+import {Text, View, TextInput, TouchableHighlight} from 'react-native';
 
 
 class Todo extends Component {
-    /**
-     * Constructor
-     */
-    constructor()
-    {
-        super();
+	/**
+	 * Constructor
+	 */
+	constructor()
+	{
+		super();
 
-        const ds = new ListViewDataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			todos  : [],
+			newTodo: ''
+		}
+	}
 
-        this.state = {
-            todos: [],
-            dataSource: ds.cloneWithRows([]),
-            newTodo: ''
-        }
-    }
+	onPressAdd = () => {
+		this.setState({ todos: [...this.state.todos, this.state.newTodo], newTodo: '' });
+	};
 
-    onPressAdd = () => {
-        this.setState({todos: [...this.state.todos, this.state.newTodo], newTodo: ''});
-    };
-
-    /**
-     * Render the Component the
-     * @returns {XML}
-     */
-    render()
-    {
-        return (
-            <View>
-                <Text>Todo:</Text>
-                <TextInput value={this.state.newTodo}
-                           onChangeText={(text) => this.setState({newTodo: text})}/>
-                <TouchableHighlight onPress={this.onPressAdd}>
-                    <Text>add</Text>
-                </TouchableHighlight>
-
-
-            </View>
-        );
-    }
+	/**
+	 * Render the Component the
+	 * @returns {XML}
+	 */
+	render()
+	{
+		return (
+			<View>
+				<Text>Todo:</Text>
+				<TextInput value={this.state.newTodo}
+						   onChangeText={(text) => this.setState({ newTodo: text })}/>
+				<TouchableHighlight onPress={this.onPressAdd}>
+					<Text>add</Text>
+				</TouchableHighlight>
+				<View>
+					{this.state.todos.map(todo => <Text>{todo}</Text>)}
+				</View>
+			</View>
+		);
+	}
 }
 
 export default Todo;
